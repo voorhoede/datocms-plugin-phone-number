c4843988163fdf3349a6d02703b4cf01ab2bdf52
Luuk <luuk@voorhoede.nl>
2025-03-03 09:41:15 +0100

    Add config options

--- src/entrypoints/FieldExtension.tsx
+++ src/entrypoints/FieldExtension.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { RenderFieldExtensionCtx } from "datocms-plugin-sdk";
 import { Canvas, FieldError } from "datocms-react-ui";
 import getValue from "../lib/get-value";
-import PhoneInput, {  isValidPhoneNumber, parsePhoneNumber } from "react-phone-number-input";
+import PhoneInput, {  getCountries, isValidPhoneNumber, parsePhoneNumber } from "react-phone-number-input";
 import { PhoneInputAdapter } from "../components/PhoneInputAdapter";
 
 import 'react-phone-number-input/style.css';
@@ -13,9 +13,25 @@ type Props = {
 };
 
 export default function FieldExtension({ ctx }: Props) {
+  const parameters = ctx.parameters;
   const fieldType = ctx.field.attributes.field_type;
   const [rawValue] = useState<string | undefined>(getValue<string>(ctx.formValues, ctx.fieldPath));
   const [value, setValue] = useState<string>();
+  const countries = getCountries()
+    .filter(country => {
+      const include = parameters.includeCountries as Array<{ label: string, value: string }>;
+      const exclude = parameters.excludeCountries as Array<{ label: string, value: string }>;
+
+      if (include && !include.find(item => item.value === country)) {
+        return false;
+      }
+
+      if (exclude && exclude.find(item => item.value === country)) {
+        return false;
+      }
+
+      return true;
+    });
 
   const handleChange = async (newValue: string | undefined) => {
     if (!isValidPhoneNumber(String(newValue))) {
@@ -38,7 +54,9 @@ export default function FieldExtension({ ctx }: Props) {
   useEffect(() => {
     if (fieldType === 'json') {
       const parsedValue = JSON.parse(String(rawValue));
-      setValue(parsedValue.number);
+      if (parsedValue) {
+        setValue(parsedValue.number);
+      }
     } else {
       setValue(rawValue as string);
     }
@@ -52,6 +70,8 @@ export default function FieldExtension({ ctx }: Props) {
         onChange={handleChange}
         inputComponent={PhoneInputAdapter}
         international={true}
+        countries={countries}
+        defaultCountry={parameters.defaultCountry?.value}
       />
 
       { !isValidPhoneNumber(String(value)) && <FieldError>Phone number is invalid</FieldError>}
--- src/entrypoints/FieldExtensionConfigScreen.tsx
+++ src/entrypoints/FieldExtensionConfigScreen.tsx
@@ -1,8 +1,8 @@
 import { RenderManualFieldExtensionConfigScreenCtx } from "datocms-plugin-sdk";
-import { MultiValue } from "react-select";
+import { MultiValue, SingleValue } from "react-select";
 import { CountryCode } from "libphonenumber-js";
 import { Canvas, Form, SelectField } from "datocms-react-ui";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getCountries } from "react-phone-number-input";
 
 type Props = {
@@ -10,23 +10,62 @@ type Props = {
   ctx: RenderManualFieldExtensionConfigScreenCtx;
 };
 
-type SelectOption = MultiValue<{
-  value: CountryCode;
-  label: CountryCode;
-}> | undefined;
-
 export default function FieldExtensionConfigScreen({ ctx }: Props) {
   const countries = getCountries().map((country) => ({
     value: country,
     label: country,
   }));
-  const [includeCountries, setIncludeCountries] = useState<SelectOption>();
+  const [defaultCountryOptions, setDefaultCountryOptions] = useState<
+    MultiValue<{
+      value: CountryCode;
+      label: CountryCode;
+    }>
+  >(countries);
+  const [includeCountries, setIncludeCountries] = useState<
+    MultiValue<{
+      value: CountryCode;
+      label: CountryCode;
+    }>
+  >(ctx.parameters.includeCountries || []);
+  const [excludeCountries, setExcludeCountries] = useState<
+    MultiValue<{
+      value: CountryCode;
+      label: CountryCode;
+    }>
+  >(ctx.parameters.excludeCountries || []);
+  const [defaultCountry, setDefaultCountry] = useState<
+    SingleValue<{
+      value: CountryCode;
+      label: CountryCode;
+    }>
+  >(ctx.parameters.defaultCountry);
 
-  const handleChange = (
-    newValue: SelectOption,
-  ) => {
-    setIncludeCountries(newValue);
-  };
+  useEffect(() => {
+    ctx.setParameters({
+      ...ctx.parameters,
+      includeCountries,
+      excludeCountries,
+      defaultCountry,
+    });
+  }, [includeCountries, excludeCountries, defaultCountry]);
+
+  useEffect(() => {
+    if (includeCountries.length > 0) {
+      setDefaultCountryOptions(includeCountries);
+      return;
+    }
+    if (excludeCountries.length > 0) {
+      const filteredCountries = countries.filter((country) => {
+        return !excludeCountries.find(
+          (excludeCountry) => excludeCountry.value === country.value,
+        );
+      });
+      setDefaultCountryOptions(filteredCountries);
+      return;
+    }
+    setDefaultCountryOptions(countries);
+    return;
+  }, [includeCountries, excludeCountries]);
 
   return (
     <Canvas ctx={ctx}>
@@ -35,13 +74,45 @@ export default function FieldExtensionConfigScreen({ ctx }: Props) {
           id="include_countries"
           label="Include countries"
           name="include_countries"
-          hint="Leave empty to include all countries"
+          hint={
+            excludeCountries.length > 0
+              ? "Empty Exclude countries if you want to include countries"
+              : "Leave empty to include all countries"
+          }
           selectInputProps={{
             isMulti: true,
             options: countries,
+            isDisabled: excludeCountries.length > 0,
           }}
           value={includeCountries}
-          onChange={handleChange}
+          onChange={(value) => setIncludeCountries(value)}
+        />
+        <SelectField
+          id="exclude_countries"
+          label="Exclude countries"
+          name="exclude_countries"
+          hint={
+            includeCountries.length > 0
+              ? "Empty Include countries if you want to exclude countries"
+              : "Leave empty to exclude no countries"
+          }
+          selectInputProps={{
+            isMulti: true,
+            options: countries,
+            isDisabled: includeCountries.length > 0,
+          }}
+          value={excludeCountries}
+          onChange={(value) => setExcludeCountries(value)}
+        />
+        <SelectField
+          id="default_country"
+          label="Default country"
+          name="default_country"
+          selectInputProps={{
+            options: defaultCountryOptions,
+          }}
+          value={defaultCountry}
+          onChange={(value) => setDefaultCountry(value)}
         />
       </Form>
     </Canvas>
